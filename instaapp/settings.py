4172c59dc4101148d155bfcfc3f5c92f9485d784
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-24 13:16:30 +0300

    configure DB settings

--- instaapp/settings.py
+++ instaapp/settings.py
@@ -111,17 +111,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'instaapp.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'instagram',
-#         'USER': 'blackrose',
-#         'PASSWORD': 'alisak6996',
-#     }
-# }
 
 
 # Password validation
